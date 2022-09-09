61d8d1106893f6ce02bb12347c079986ad27fbd4
remindev <reminremin01@gmail.com>
2022-09-09 22:26:57 +0530

    Simplified

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 
-function bannerChanger(){
+function bannerChanger(len){
+    // this function creates a loop repeats at specified time delay
+    // then with that we make image scrolling transition
 
-    let imgCont = document.getElementById("banner-cont");
-    let num = 100;
+    let imgCont = document.getElementById("banner-cont"); // image container
+    let num = 100; // value to move the image left 
+    let limit = len*5; // making limit Eg. len = 5 then limit will be 500
 
     let time = setInterval(() => {
+        // sets an time intervel loop
         
-        imgCont.style.transform = 'translateX(-'+num+'%)';
+        imgCont.style.transform = 'translateX(-'+num+'%)'; // moving images using transition
 
-        num=num+100;
+        num=num+100; // adding the value of num for keep moving towards left
 
-        if(num==500){
-            // clearInterval(time);
-            num = 0;
+        if(num==limit){
+            // checks if reach the end of the images || last image
+
+            num = 0; // reset the value of num
         };
 
-        console.log("SOMETHING IS WORKING");
-
-    }, 5000);
+    }, 5000); // here the 5000 is 5s || delay time
 
 }
 
-bannerChanger();
+bannerChanger(5); // calling function with total number of images
